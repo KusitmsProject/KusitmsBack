b521432296e0ef8e8b3e8c5bda546c109edfb1ef
ympark99 <ympark1230@naver.com>
2022-10-17 13:38:27 +0900

    readme 추가, default 서버 시간 추가

--- src/main/java/com/example/demo/src/controller/TestController.java
+++ src/main/java/com/example/demo/src/controller/TestController.java
@@ -2,9 +2,15 @@ package com.example.demo.src.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
 
 @RestController
 public class TestController {
+
+    @GetMapping("/")
+    public String time() {
+        return LocalDateTime.now().toString();
+    }
 
     @GetMapping("/test")
     public String test(){
