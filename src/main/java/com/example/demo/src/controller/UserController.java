24e2a9babc680e94228a26596f30daeea8188f5d
JjungminLee <julie0964@naver.com>
2022-11-04 12:47:49 +0900

    카카오 로그인 ,db저장,swagger 추가

--- src/main/java/com/example/demo/src/controller/UserController.java
+++ src/main/java/com/example/demo/src/controller/UserController.java
@@ -1,4 +1,29 @@
-package com.example.demo.controller;
+package com.example.demo.src.controller;
 
+import com.example.demo.src.dto.GetUserDto;
+import com.example.demo.src.dto.PostUserDto;
+import com.example.demo.dto.response.BaseResponse;
+import com.example.demo.src.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/bring")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+    @Operation(summary = "로그인(진짜)", description = "/bring/login")
+    @PostMapping(value="/login")
+    public BaseResponse<GetUserDto> login(@RequestBody PostUserDto postUserDto) {
+
+        Long id=userService.saveUser(postUserDto);
+        GetUserDto getUserDto=userService.findUser(id);
+
+        return new BaseResponse<>(getUserDto);
+
+    }
 }
--- src/main/java/com/example/demo/src/service/UserService.java
+++ src/main/java/com/example/demo/src/service/UserService.java
@@ -1,4 +1,79 @@
-package com.example.demo.service;
+package com.example.demo.src.service;
 
+import com.example.demo.src.dto.GetUserDto;
+import com.example.demo.src.dto.PostUserDto;
+import com.example.demo.src.entity.User;
+import lombok.Builder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service
 public class UserService {
+    private final com.example.demo.repository.UserRepository userRepository;
+
+    public UserService(com.example.demo.repository.UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
+
+    public long saveUser(PostUserDto postUserDto) {
+
+        User user = userRepository.findByKakaoEmail(postUserDto.getKakao_email());
+        if (user == null) {
+            user = (User.builder()
+                    .kakaoId(postUserDto.getKakao_id())
+                    .kakaoEmail(postUserDto.getKakao_email())
+                    .kakaoNickname(postUserDto.getKakao_nickname())
+                    .profileImgUrl(postUserDto.getProfile_img_url())
+                    .gender(postUserDto.getGender())
+
+                    .build());
+
+            userRepository.save(user);
+
+
+        }
+        return userRepository.findByKakaoEmail(postUserDto.getKakao_email()).getUserIdx();
+    }
+
+    public long saveKakaoUser(HashMap<String, Object> userInfo){
+
+
+        User user= userRepository.findByKakaoEmail(userInfo.get("kakaoEmail").toString());
+
+
+        if(user==null){
+            user= (User.builder()
+                    .kakaoId((Long) userInfo.get("kakaoId"))
+                    .kakaoEmail(userInfo.get("kakaoEmail").toString())
+                    .kakaoNickname(userInfo.get("kakaoNickname").toString())
+                    .profileImgUrl(userInfo.get("profileImgUrl").toString())
+                    .gender(userInfo.get("gender").toString())
+
+                    .build());
+
+            userRepository.save(user);
+
+
+        }
+
+        return userRepository.findByKakaoEmail(userInfo.get("kakaoEmail").toString()).getUserIdx();
+
+
+    }
+
+    @Builder
+    public GetUserDto findUser(Long userIdx){
+
+        User user=userRepository.findByUserIdx(userIdx);
+
+        return GetUserDto.builder()
+                .userIdx(user.getUserIdx())
+                .kakao_id(user.getUserIdx())
+                .kakao_nickname(user.getKakaoNickname())
+                .profile_img_url(user.getProfileImgUrl())
+                .gender(user.getGender())
+                .build();
+
+    }
 }
