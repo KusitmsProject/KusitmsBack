574a802e51a174968d1c51678ce6d7da149fe5cd
ympark99 <ympark1230@naver.com>
2022-11-08 18:35:40 +0900

    [FIX] security error fixed

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -17,38 +17,41 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-
-
-
-    @Bean
     @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    protected void configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.httpBasic().disable();
     }
 
-    private static final String[] AUTH_WHITELIST = {
-            "/swagger-resources/**",
-            "/swagger-ui.html",
-            "/v2/api-docs",
-            "/webjars/**"
-    };
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(AUTH_WHITELIST);
-    }
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.csrf().disable();
-        //http.httpBasic().disable(); // 일반적인 루트가 아닌 다른 방식으로 요청시 거절, header에 id, pw가 아닌 token(jwt)을 달고 간다. 그래서 basic이 아닌 bearer를 사용한다.
-        http
-                .authorizeRequests()// 요청에 대한 사용권한 체크
-                .antMatchers("/bring/**","/v3/api-docs/**","/**",
-                        "/swagger-ui/**").permitAll() // 가입 및 인증 주소는 누구나 접근가능
-                .antMatchers("/test").hasAnyRole("USER", "ADMIN")
-                .anyRequest().hasRole("USER") // 그외 나머지 요청은 모두 인증된 회원만 접근 가능
-                .and();
-
-    }
+//    @Bean
+//    @Override
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+//
+//    private static final String[] AUTH_WHITELIST = {
+//            "/swagger-resources/**",
+//            "/swagger-ui.html",
+//            "/v2/api-docs",
+//            "/webjars/**"
+//    };
+//
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().antMatchers(AUTH_WHITELIST);
+//    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http.csrf().disable();
+//        //http.httpBasic().disable(); // 일반적인 루트가 아닌 다른 방식으로 요청시 거절, header에 id, pw가 아닌 token(jwt)을 달고 간다. 그래서 basic이 아닌 bearer를 사용한다.
+//        http
+//                .authorizeRequests()// 요청에 대한 사용권한 체크
+//                .antMatchers("/bring/**","/v3/api-docs/**","/**",
+//                        "/swagger-ui/**").permitAll() // 가입 및 인증 주소는 누구나 접근가능
+//                .antMatchers("/test").hasAnyRole("USER", "ADMIN")
+//                .anyRequest().hasRole("USER") // 그외 나머지 요청은 모두 인증된 회원만 접근 가능
+//                .and();
+//
+//    }
 }
