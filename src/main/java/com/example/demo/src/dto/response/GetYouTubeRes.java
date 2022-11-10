6bdbdf5d5c4a9f94806256e323ff810d25e39120
JjungminLee <julie0964@naver.com>
2022-11-11 06:44:03 +0900

    [FEAT] 유튜브 videoId 조회 기능 추가

--- src/main/java/com/example/demo/src/dto/response/GetYouTubeRes.java
+++ src/main/java/com/example/demo/src/dto/response/GetYouTubeRes.java
@@ -1,8 +1,23 @@
 package com.example.demo.src.dto.response;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+
 public class GetYouTubeRes {
 
     private String videoId;
     private String videoTitle;
+    private String videoURL;
+
+    @Builder
+    public GetYouTubeRes(String videoId,String videoTitle,String videoURL){
+        this.videoId=videoId;
+        this.videoTitle=videoTitle;
+        this.videoURL=videoURL;
+    }
+
+
 
 }
