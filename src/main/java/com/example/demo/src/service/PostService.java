fe2ac52ca400cbfdda6ffe53267415f83fb35f43
JjungminLee <julie0964@naver.com>
2022-11-14 03:03:39 +0900

    [FIX] 동일 이름 노래 처리 (track,artist로 구분)

--- src/main/java/com/example/demo/src/service/PostService.java
+++ src/main/java/com/example/demo/src/service/PostService.java
@@ -3,6 +3,7 @@ package com.example.demo.src.service;
 import com.example.demo.src.dto.request.PostPostingReq;
 import com.example.demo.src.dto.response.BaseException;
 import com.example.demo.src.dto.response.PostPostingRes;
+import com.example.demo.src.entity.Music;
 import com.example.demo.src.entity.Post;
 import com.example.demo.src.repository.MusicRepository;
 import com.example.demo.src.repository.PostRepository;
@@ -10,6 +11,9 @@ import com.example.demo.src.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -27,11 +31,21 @@ public class PostService {
     }
 
     public PostPostingRes register(PostPostingReq postPostingReq) throws BaseException {
+        //jungmin
+        // 이문세의 붉은 노을 빅뱅의 붉은 노을을 구별해야함
+        // track으로 찾아온 Music 의 artist와 artist로 찾아온 Music의 artist가 같으면 저장
+
+        List<Music> findByTrack = musicRepository.findByTrack(postPostingReq.getTrack());
+        List<Music>ans=findByTrack.stream().filter(
+                o->o.getArtist().equals(postPostingReq.getArtist())
+        ).collect(Collectors.toList());
+        Music music=ans.get(0);//filter한 값 중 가장 앞에 값을 music에 넣자
+
         Post post = Post.builder()
                 .user(userRepository.findByUserIdx(postPostingReq.getUserIdx()))
                 .date(postPostingReq.getDate())
                 .emotion(postPostingReq.getEmotion())
-                .music(musicRepository.findByTrack(postPostingReq.getTrack()))
+                .music(music)
                 .weather(postPostingReq.getWeather())
                 .season(postPostingReq.getSeason())
                 .lyrics(postPostingReq.getLyrics())
