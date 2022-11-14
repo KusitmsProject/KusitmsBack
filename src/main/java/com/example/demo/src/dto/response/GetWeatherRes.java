95246014b9211d8fc6d8cefc9c2c5b2a1b31d68b
ympark99 <ympark1230@naver.com>
2022-11-15 00:02:05 +0900

    [ADD] 가장 가까운 거리 탐색 알고리즘 구현 중

--- src/main/java/com/example/demo/src/dto/response/GetWeatherRes.java
+++ src/main/java/com/example/demo/src/dto/response/GetWeatherRes.java
@@ -3,14 +3,15 @@ package com.example.demo.src.dto.response;
 import lombok.*;
 
 @Data
-@Getter
 public class GetWeatherRes {
 
+    private String date; // 날짜
     private String weather; // 비, 눈, 맑음, 흐림
     private Integer temp; // 0 : 그외 1 : 더움, 2 : 추움
 
     @Builder
-    public GetWeatherRes(String weather, Integer temp) {
+    public GetWeatherRes(String date, String weather, Integer temp) {
+        this.date = date;
         this.weather = weather;
         this.temp = temp;
     }
--- src/main/java/com/example/demo/src/service/WeatherService.java
+++ src/main/java/com/example/demo/src/service/WeatherService.java
@@ -21,9 +21,13 @@ import java.util.List;
 @Transactional
 @PropertySource("classpath:weather.properties")
 public class WeatherService {
+    private static String apikey; // 기상청 api key
 
-    @Value("${api_key}")
-    private static String apikey;
+    // static에 @Value 안되므로 주입
+    @Value("${key}")
+    public void setApikey(String key) {
+        apikey = key;
+    }
 
     // tag값의 정보를 가져오는 함수
     public static String getTagValue(String tag, Element eElement) {
@@ -41,16 +45,56 @@ public class WeatherService {
         return result;
     }
 
+    // 해당 위치와 가장 가까운 관측소 찾아주는 함수
+    public static String getNearObservatory(String place){
+        String Observatories[] = {"속초","철원","동두천","파주","대관령","춘천","백령도","강릉","동해",
+                "서울","인천","원주","울릉도","홍천","태백","정선","제천","보은","천안","보령","부여","금산",
+                "세종","부안","임실","정읍","남원","장수","수원","영월","충주","서산","울진","청주","대전",
+                "추풍령","안동","상주","포항","군산","대구","전주","울산","창원","광주","부산","고창",
+                "영광","김해","순창","양산","보성","강진","장흥","해남","고흥","의령","함양",
+                "광양","진도","봉화","영주","문경","통영","목포","여수","흑산도","완도","고창","순천",
+                "홍성","제주","고산","성산","서귀포","진주","강화","양평","이천","인제","청송","영덕",
+                "의성","구미","영천","경주","거창","합천","밀양","산청","거제","남해"};
+
+        String ObserNum[] = {"90","95","98","99","100","101","102","105","106","108",
+                "112","114","115","212","216","217","221","226","232","235","236","238",
+                "239","243","244","245","247","248","119","121","127","129","130","131",
+                "133","135","136","137","138","140","143","146","152","155","156","159",
+                "251","252","253","254","257","258","259","260","261","262","263","264",
+                "266","268","271","272","273","162","165","168","169","170","172","174",
+                "177","184","185","188","189","192","201","202","203","211","276","277",
+                "278","279","281","283","284","285","288","289","294","295"};
+
+        String[] inputs = place.split(" ");
+
+        for(int i = 0; i < inputs.length; i++){
+            if(i > 1) break; // 시군 단위까지 검색
+            for(int j = 0; j < Observatories.length; j++){
+                String str = inputs[i];
+
+                // todo : 시, 광역시, 군 등 처리
+//                String str = inputs[i].substring(0, inputs[i].length()-1);
+                if(str.indexOf(Observatories[j]) != -1)
+                    return ObserNum[j];
+            }
+        }
+
+        return "";
+    }
+
     public static List<GetWeatherRes> weatherSearch(String date, String place) throws BaseException {
         List<GetWeatherRes> result = new ArrayList<>();
 
         try{
-            String placeNum = "108";
+            String test = getNearObservatory(place);
+            System.out.println("test = " + test);
+            String placeNum = "108"; // 장소 번호
 
             // url (현재 기능상 시작, 종료날짜 통일)
             String url = "http://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList?serviceKey=" + apikey
                     + "&numOfRows=10&pageNo=1&dataCd=ASOS&dateCd=DAY&startDt=" + date + "&endDt=" + date + "&stnIds=" + placeNum;
 
+            System.out.println("url = " + url);
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(url);
@@ -63,50 +107,43 @@ public class WeatherService {
 
             for(int temp = 0; temp < nList.getLength(); temp++){
                 Node nNode = nList.item(temp);
-
                 Element eElement = (Element) nNode;
 
 //                System.out.println("위치 : " + getTagValue("stnNm", eElement));
 //                System.out.println("위치번호 : " + getTagValue("stnId", eElement));
                 System.out.println("날짜 : " + getTagValue("tm", eElement));
-//                System.out.println("평균 기온 : " + getTagValue("avgTa", eElement));
-//                System.out.println("최저 기온 : " + getTagValue("minTa", eElement));
-//                System.out.println("최고 기온 : " + getTagValue("maxTa", eElement));
-//                System.out.println("강수 지속시간 : " + getTagValue("sumRnDur", eElement));
-//                System.out.println("일 강수량 mm : " + getTagValue("sumRn", eElement));
-//                System.out.println("평균 풍속 m/s : " + getTagValue("avgWs", eElement));
-//                System.out.println("평균 상대습도 % : " + getTagValue("avgRhm", eElement));
-//                System.out.println("평균 전운량 (10분위) : " + getTagValue("avgTca", eElement));
-//                System.out.println("일 최심신적설(눈 cm) : " + getTagValue("ddMefs", eElement));
+                System.out.println("평균 기온 : " + getTagValue("avgTa", eElement));
+                System.out.println("최고 기온 : " + getTagValue("maxTa", eElement));
                 System.out.println("일기현상 : " + getTagValue("iscs", eElement));
+
+                GetWeatherRes item = new GetWeatherRes(date, "", 0);
 
                 // 날씨 구분
                 String cloud = getTagValue("avgTca", eElement);
                 String weather = getTagValue("iscs", eElement);
                 if(weather.indexOf("눈") != -1)
-                    System.out.println("눈옴");
+                    item.setWeather("눈");
                 else if(weather.indexOf("비") != -1 || weather.indexOf("소나기") != -1)
-                    System.out.println("비옴");
-                    // 전운량 7이상이면 흐림 처리 (기상청 기준 0~5 맑음, 6~8 구름많음, 9~10 흐림)
+                    item.setWeather("비");
+                // 전운량 7이상이면 흐림 처리 (기상청 기준 0~5 맑음, 6~8 구름많음, 9~10 흐림)
                 else if(Float.parseFloat(cloud) >= 7)
-                    System.out.println("흐림");
-                else System.out.println("맑음");
+                    item.setWeather("흐림");
+                else item.setWeather("맑음");
 
                 // 더움 추분 구분
                 String maxTemp = getTagValue("maxTa", eElement);
                 String avgTemp = getTagValue("avgTa", eElement);
                 if(Float.parseFloat(maxTemp) >= 30)
-                    System.out.println("더움");
+                    item.setTemp(1);
                 else if(Float.parseFloat(avgTemp) < 0)
-                    System.out.println("추움");
+                    item.setTemp(2);
 
+                result.add(item);
             }
 
         } catch (Exception e){
             e.printStackTrace();
         }
-
-
 
         return result;
     }
